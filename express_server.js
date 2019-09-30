f8e7a4bbb25b07fde1fec06a2d711d19a61b0e5a
Karen <karendavtyan21@mail.com>
2019-09-30 21:14:09 +0000

    got redirect to a legit url from my short url

--- express_server.js
+++ express_server.js
@@ -4,12 +4,21 @@ const PORT = 8080;
 app.set('view engine', 'ejs');
 
 function generateRandomString() {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (let i = 0; i < 6; i++) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
 
-}
-
+  const bodyParser = require('body-parser');
+  app.use(bodyParser.urlencoded({extended: true}));
+  
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": "www.lighthouselabs.ca",
+  "9sm5xK": "www.google.com",
 };
 
 app.get('/urls', (req, res) => {
@@ -24,11 +33,8 @@ app.get('/urls/new', (req, res) => {
 app.get('/urls/:shortURL', (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
   res.render('urls_show', templateVars)
-})
 
-app.get('/', (req, res) => {
-  res.send('Hello!');
-});
+})
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
@@ -39,12 +45,18 @@ app.get('/hello', (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  let shortURL = generateRandomString()
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`)
+  res.send(shortURL)
+  res.status(200).send();
 });
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = 'http://' + urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
