d10a2cac1a2c24982fe26996bb3789021bb469a1
Karen <karendavtyan21@mail.com>
2019-09-30 19:59:42 +0000

    adding redirect

--- express_server.js
+++ express_server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
-
 app.set('view engine', 'ejs');
+
+function generateRandomString() {
+
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -12,6 +15,10 @@ const urlDatabase = {
 app.get('/urls', (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
+});
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
 });
 
 app.get('/urls/:shortURL', (req, res) => {
@@ -30,6 +37,14 @@ app.get('/urls.json', (req, res) => {
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n')
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
