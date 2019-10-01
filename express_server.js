0cd1eba2edfb1134dcea18dc4d39d5390fc67437
Karen <karendavtyan21@mail.com>
2019-10-01 19:32:06 +0000

    got cookies and login and logout on the website

--- express_server.js
+++ express_server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser')
 const PORT = 8080;
 app.set('view engine', 'ejs');
-
+app.use(cookieParser())
 function generateRandomString() {
     var result           = '';
     var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -22,16 +23,19 @@ const urlDatabase = {
 };
 
 app.get('/urls', (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { 
+    username: req.cookies["username"],
+    urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
 
 app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"]}
+  res.render('urls_new', templateVars);
 });
 
 app.get('/urls/:shortURL', (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"]}
   res.render('urls_show', templateVars)
 
 })
@@ -69,6 +73,16 @@ app.get("/u/:shortURL", (req, res) => {
   const longURL = 'http://' + urlDatabase[req.params.shortURL]
   res.redirect(longURL);
 });
+
+app.post("/login", (req, res) => {
+  res.cookie('username', req.body.username)
+  res.redirect(`/urls`)
+})
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username', req.body.username)
+  res.redirect('/urls')
+})
 
 
 app.listen(PORT, () => {
